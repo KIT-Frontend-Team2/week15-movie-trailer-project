c195a29518ebdfa389a210c2bab751295f1d87ab
Jihyeong00 <iolpd42@gmail.com>
2023-06-20 11:47:34 +0900

    🌈feat : Initial Setting

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  endOfLine: "lf",
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: false,
+  tabWidth: 2,
+  useTabs: true,
+  trailingComma: "all",
+  arrowParens: "avoid",
+};
+

c1f7ca9f1d2871806d971e794352c3c309c58da6
naturej <natu_rej@naver.com>
2023-06-23 00:02:07 +0900

    🌈feat : global css, theme 추가

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import router from 'routes/routes'
 import './App.css'
 import { RouterProvider } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
+import GlobalStyles from 'styles/global'
+import theme from 'styles/theme'
 
 function App() {
 	const routing = router
 
-	return <RouterProvider router={routing} />
+	return (
+		<ThemeProvider theme={theme}>
+			<GlobalStyles />
+			<RouterProvider router={routing} />
+		</ThemeProvider>
+	)
 }
 
 export default App

13ce9c0c1fd1019cf9eb0d632b55d6bfcebda59e
naturej <natu_rej@naver.com>
2023-06-25 17:22:40 +0900

    🌈feat : 상세 페이지 수정

--- src/styles/common.js
+++ src/styles/common.js
@@ -5,6 +5,20 @@ export const flexCenter = css`
 	justify-content: center;
 	align-items: center;
 `
+export const flexBetween = css`
+	display: flex;
+	justify-content: space-between;
+`
+export const flexBetweenCenter = css`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+`
+export const flexBetweenEnd = css`
+	display: flex;
+	justify-content: space-between;
+	align-items: flex-end;
+`
 export const positionInset = css`
 	position: absolute;
 	inset: 0;
